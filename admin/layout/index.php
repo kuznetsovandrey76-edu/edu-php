842bc7ff88dbc4b9699d91bb6ac9b60f6fbefe50
Andrey Kuznetsov <and.rey.q@yandex.ru>
2019-01-31 11:39:20 +0300

    update admin folder

---
+++ admin/layout/index.php
@@ -0,0 +1,41 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbName = "edu-php";
+
+$link = mysqli_connect($host, $user, $password, $dbName);
+mysqli_query($link, "SET NAMES 'utf8'");
+
+
+$query = "SELECT * FROM pages";
+$result = mysqli_query($link, $query) or die(mysqli_erorr($link));
+for($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+
+$title = 'Admin web page';
+$content = "
+<table>
+    <tr>
+        <th>title</th>
+        <th>url</th>
+        <th>edit</th>
+        <th>delete</th>
+    </tr>";
+foreach ($data as $page) {
+    $content .= "
+    <tr>
+        <td>{$page['title']}</td>
+        <td>{$page['url']}</td>
+        <td>edit</td>
+        <td>delete</td>
+    </tr>";
+}
+$content .= '</table>';
+
+
+include "layout.php";
+
